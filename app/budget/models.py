08d6943e9db112faf3dee608f69e3160336c631b
eltaweelpower2020 <mr.eltaweel@gmail.com>
2022-04-22 21:07:38 +0200

    docker configurations

--- app/budget/models.py
+++ app/budget/models.py
@@ -9,13 +9,11 @@ class LibraBudget(models.Model):
     )
     added_by=models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
     money_from=models.CharField(max_length=200)
-    reason=models.CharField(max_length=200,blank=True,null=True)
     money_to=models.CharField(max_length=200)
     amount=models.FloatField()
     location=models.CharField(max_length=20, choices=SITES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
     
      
 
