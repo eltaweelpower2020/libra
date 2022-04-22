43b4342a1bbf77db8d76b48646fe08a71a5e8b4e
eltaweelpower2020 <mr.eltaweel@gmail.com>
2022-04-22 22:06:59 +0200

    docker configurations

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 # python manage.py migrate --fake shirts
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
 
 # python manage.py shell -c "from users.models import User; User.objects.create_superuser('admin4', 'admin@example.com', '123qwe456asd')"
