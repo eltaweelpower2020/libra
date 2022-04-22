fb55c0e3e53f6b6b3bfb038d96ed68e186f5945d
eltaweelpower2020 <mr.eltaweel@gmail.com>
2022-04-22 20:32:09 +0200

    docker configurations

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,7 +6,7 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 
-# python manage.py shell -c "from users.models import User; User.objects.create_superuser('admin4', 'admin@example.com', '123qwe456asd')"
+python manage.py shell -c "from users.models import User; User.objects.create_superuser('admin4', 'admin@example.com', '123qwe456asd')"
 
 
 uwsgi --socket :8000 --master --enable-threads -b 32768 --module libratracking.wsgi 
