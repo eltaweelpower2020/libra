4c62bf065b5e52f4a90c7682f1f8e0856908d42e
eltaweelpower2020 <mr.eltaweel@gmail.com>
2022-04-22 21:22:00 +0200

    docker configurations

--- app/budget/models.py
+++ app/budget/models.py
@@ -15,6 +15,9 @@ class LibraBudget(models.Model):
     location=models.CharField(max_length=20, choices=SITES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.added_by
     
     
     
